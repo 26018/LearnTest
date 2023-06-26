7831e9b8f79258e01e1d61a151a296ba27e4f791
Ljh <2033471349@qq.com>
2023-06-26 20:53:59 +0800

    添加dev map代码

---
+++ src/main/java/top/fixyou/entity/devMapTest.java
@@ -0,0 +1,11 @@
+package top.fixyou.entity;
+
+/**
+ * @author Ljh
+ * @since 2023/6/26 20:53
+ */
+public class devMapTest {
+    public static void main(String[] args) {
+        System.out.println("this is dev map test");
+    }
+}
