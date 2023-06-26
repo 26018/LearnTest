aae481d870fb16e101fa9bcf22120df306b8d2cc
Ljh <2033471349@qq.com>
2023-06-26 20:34:45 +0800

    添加新的功能

---
+++ src/main/java/top/fixyou/entity/GitTest.java
@@ -0,0 +1,13 @@
+package top.fixyou.entity;
+
+/**
+ * @author Ljh
+ * @since 2023/6/26 20:34
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+
+}
